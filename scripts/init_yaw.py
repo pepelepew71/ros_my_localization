85595cefa4845c57194db7ea5f75ca82b870db11
yc.huang71 <yc.huang71@gmail.com>
2020-06-24 22:18:34 +0800

    make all launch arg yaml default to empty
    for preventing potential bug

--- scripts/init_yaw.py
+++ scripts/init_yaw.py
@@ -28,25 +28,36 @@ def _cb_sub(msg):
     QUAT = msg.orientation
 
 def cb_service(request):
+
     sub_once = rospy.Subscriber(name=NAME_IMU, data_class=Imu, callback=_cb_sub)
     rate = rospy.Rate(hz=1)
+
     while True:
+
         if QUAT:
             p = SetPoseRequest()
-            p.pose.header.frame_id = NAME_FRAME
+            p.pose.header.frame_id = NAME_FRAME_ID_ODOM
             p.pose.header.stamp = rospy.Time.now()
             p.pose.pose.pose.orientation.x = QUAT.x
             p.pose.pose.pose.orientation.y = QUAT.y
             p.pose.pose.pose.orientation.z = QUAT.z
             p.pose.pose.pose.orientation.w = QUAT.w
-            SERVICE.call(p)
+
+            SERVICE_ODOM.call(p)
+
+            if SERVICE_MAP:
+                p.pose.header.frame_id = NAME_FRAME_ID_MAP
+                SERVICE_MAP.call(p)
+
             _, _, rz_rad = euler_from_quaternion((QUAT.x, QUAT.y, QUAT.z, QUAT.w))
             rospy.loginfo("init_state: setup yaw = {}".format(rz_rad))
             sub_once.unregister()
             break
         else:
             pass
+
         rate.sleep()
+
     return EmptyResponse()
 
 if __name__ == "__main__":
@@ -54,14 +65,24 @@ if __name__ == "__main__":
     rospy.init_node(name='init_yaw', anonymous=False)
 
     # -- Get parameters
-    NAME_IMU = rospy.get_param(param_name="~name_imu", default="/imu")
-    NAME_FRAME = rospy.get_param(param_name="~name_frame", default="")
-    name_set_pose = rospy.get_param(param_name="~name_set_pose", default="")
+    NAME_IMU = rospy.get_param(param_name="~topic_imu", default="/imu")
+
+    name_set_pose_odom = rospy.get_param(param_name="~service_set_pose_odom", default="")
+    NAME_FRAME_ID_ODOM = rospy.get_param(param_name="~frame_id_odom", default="")
+
+    name_set_pose_map = rospy.get_param(param_name="~service_set_pose_map", default="")
+    NAME_FRAME_ID_MAP = rospy.get_param(param_name="~frame_id_map", default="")
 
     # -- Get ServiceProxy
-    SERVICE = rospy.ServiceProxy(name=name_set_pose, service_class=SetPose)
+    SERVICE_ODOM = None
+    if name_set_pose_odom:
+        SERVICE_ODOM = rospy.ServiceProxy(name=name_set_pose_odom, service_class=SetPose)
+
+    SERVICE_MAP = None
+    if name_set_pose_map:
+        SERVICE_MAP = rospy.ServiceProxy(name=name_set_pose_map, service_class=SetPose)
 
     # -- Node function
-    rospy.Service(name="~set_yaw", service_class=Empty, handler=cb_service)
+    rospy.Service(name="~set", service_class=Empty, handler=cb_service)
 
     rospy.spin()
